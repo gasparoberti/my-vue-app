80d68ee1d70ef2147768dda31ea070c008b2ede9
Gaspar <obertigaspar@gmail.com>
2022-11-04 16:44:40 -0300

    Log period for each selection

---
+++ src/components/Timeline.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+const periods = ["Today", "This Week", "This Month"]
+
+const selectedPeriod = ref("Today")
+
+function selectPeriod(period: string) {
+    // console.log(period);
+    selectedPeriod.value = period
+}
+</script>
+
+<template>
+    <nav class="is-primary panel">
+        {{ selectedPeriod }}
+        <span class="panel-tabs">
+            <a v-for="period of periods" :key="period" v-on:click="selectPeriod(period)">
+                <!--el v-on:click es lo mismo que @click="selectPeriod(period)" -->
+                {{ period }}
+            </a>
+        </span>
+    </nav>
+</template>
+
+<style>
+
+</style>
